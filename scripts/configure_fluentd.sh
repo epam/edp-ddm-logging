7c0beae126e812dcf59cd9becff632e0fae8345c
Dmytro Zinchenko <dmytro_zinchenko1@epam.com>
2022-09-12 15:50:21 +0300

    [EDP-DDM-18584] Fix ds name depending on cluster version
    Change-Id: Ia407cf6987575f6adc640cf2411dc6b6820a4bca

--- scripts/configure_fluentd.sh
+++ scripts/configure_fluentd.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-while [[ `oc get ds/fluentd -n openshift-logging --no-headers --ignore-not-found` == '' ]]; do
+DAEMON_SET_NAME=collector
+CLUSTER_VERSION=$(oc get clusterversion --no-headers | awk '{print $2}' | awk -F- '{print $1}')
+if [[ "${CLUSTER_VERSION}" == '4.7.0' ]]
+then
+  DAEMON_SET_NAME=fluentd
+fi
+
+DAEMON_SET_NAME
+while [[ `oc get ds/${DAEMON_SET_NAME} -n openshift-logging --no-headers --ignore-not-found` == '' ]]; do
   echo "Waiting for ds/fluentd becomes available, sleep for 10 sec"
   sleep 10
 done
@@ -12,8 +20,8 @@ echo "Applying fluentd config"
 oc replace -f ../scripts/fluentd_conf.yaml
 
 echo "Setting ENV vars for fluentd DS"
-oc -n openshift-logging set env ds/fluentd MERGE_JSON_LOG=true FILE_BUFFER_LIMIT=768Mi BUFFER_QUEUE_FULL_ACTION=drop_oldest_chunk \
+oc -n openshift-logging set env ds/${DAEMON_SET_NAME} MERGE_JSON_LOG=true FILE_BUFFER_LIMIT=768Mi BUFFER_QUEUE_FULL_ACTION=drop_oldest_chunk \
     BUFFER_QUEUE_LIMIT=64 BUFFER_SIZE_LIMIT=16777216 PRESERVE_JSON_LOG=true CDM_KEEP_EMPTY_FIELDS=message
 
 echo "Force restarting pods"
-oc patch  -n openshift-logging ds/fluentd --type='json' -p="{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"update_timestamp\":\"$(date +%s)\"}}}}}" --type=merge
+oc patch  -n openshift-logging ds/${DAEMON_SET_NAME} --type='json' -p="{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"update_timestamp\":\"$(date +%s)\"}}}}}" --type=merge
