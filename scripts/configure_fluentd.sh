8d6afdbdb51dfc8390281e81a0aada85c79da641
Dmytro Zinchenko <dmytro_zinchenko1@epam.com>
2022-09-23 13:37:04 +0300

    [EDP-DDM-0000] Fix parsing json logs for newer logging operator
    Change-Id: I0e35a0993e0e6213925b3e9d7e39bfa6ba60d96b

--- scripts/configure_fluentd.sh
+++ scripts/configure_fluentd.sh
@@ -9,17 +9,11 @@ fi
 
 DAEMON_SET_NAME
 while [[ `oc get ds/${DAEMON_SET_NAME} -n openshift-logging --no-headers --ignore-not-found` == '' ]]; do
-  echo "Waiting for ds/fluentd becomes available, sleep for 10 sec"
+  echo "Waiting for ds/${DAEMON_SET_NAME} becomes available, sleep for 10 sec"
   sleep 10
 done
 
-echo "Switching logging to 'Unmanaged' state"
-oc patch -n openshift-logging ClusterLogging instance  --type='json' -p='{"spec":{"managementState":"Unmanaged"}}' --type=merge
-
-echo "Applying fluentd config"
-oc replace -f ../scripts/fluentd_conf.yaml
-
-echo "Setting ENV vars for fluentd DS"
+echo "Setting ENV vars for fluentd/collector DS"
 oc -n openshift-logging set env ds/${DAEMON_SET_NAME} MERGE_JSON_LOG=true FILE_BUFFER_LIMIT=768Mi BUFFER_QUEUE_FULL_ACTION=drop_oldest_chunk \
     BUFFER_QUEUE_LIMIT=64 BUFFER_SIZE_LIMIT=16777216 PRESERVE_JSON_LOG=true CDM_KEEP_EMPTY_FIELDS=message
 
